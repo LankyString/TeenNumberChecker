e2248b6bed477c1316c43e2a10b00c5c9648dbfd
Andrew <39748224+LankyString@users.noreply.github.com>
2019-03-26 19:43:09 +0000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        boolean a =TeenNumberChecker.hasTeen(23,54,18);
+        System.out.println(a);
+        boolean b = TeenNumberChecker.isTeen(13);
+        System.out.println(b);
+    }
+}
